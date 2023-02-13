5af40ade849ec1fdc355a9433ef086867cbb74be
지찬우 <jcw001031@gmail.com>
2023-02-13 16:22:19 +0900

    refactor - 다대일 연관관계 fetch 전략 LAZY 설정

--- app_server/src/main/java/knu/networksecuritylab/appserver/entity/book/BookTag.java
+++ app_server/src/main/java/knu/networksecuritylab/appserver/entity/book/BookTag.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,10 +24,11 @@ public class BookTag {
     @Column(name = "book_tag_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id")
     private Tag tag;
 
