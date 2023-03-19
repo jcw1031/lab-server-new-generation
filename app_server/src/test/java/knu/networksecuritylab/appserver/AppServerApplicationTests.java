db35ae5598accfc6cf8ba773cc10acac3492c66b
지찬우 <jcw001031@gmail.com>
2023-03-19 18:02:16 +0900

    토큰 암호키 암호화 설정

--- app_server/src/test/java/knu/networksecuritylab/appserver/AppServerApplicationTests.java
+++ app_server/src/test/java/knu/networksecuritylab/appserver/AppServerApplicationTests.java
@@ -1,5 +1,6 @@
 package knu.networksecuritylab.appserver;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,19 @@ class AppServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		System.out.println(jasyptEncoding("KNU_NETWORK_SECURITY_LAB"));
+	}
+
+	public String jasyptEncoding(String value) {
+		String key = "LAB_SECURITY_JASYPT_KEY";
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+		pbeEnc.setPassword(key);
+		return pbeEnc.encrypt(value);
 	}
 
 }
