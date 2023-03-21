24df041a1a4b8163a131cf60b4aa9539c90676f5
지찬우 <jcw001031@gmail.com>
2023-03-21 22:12:18 +0900

    Test 제거

--- app_server/src/test/java/knu/networksecuritylab/appserver/AppServerApplicationTests.java
+++ app_server/src/test/java/knu/networksecuritylab/appserver/AppServerApplicationTests.java
@@ -1,27 +1,12 @@
 package knu.networksecuritylab.appserver;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void test() {
-		System.out.println(jasyptEncoding("KNU_NETWORK_SECURITY_LAB"));
-	}
-
-	public String jasyptEncoding(String value) {
-		String key = "LAB_SECURITY_JASYPT_KEY";
-		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-		pbeEnc.setPassword(key);
-		return pbeEnc.encrypt(value);
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
